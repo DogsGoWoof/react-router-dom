cd62fe895020db9db60e9232e7dc7b0ddc5bb812
jason <jason_tan75@hotmail.com>
2024-07-19 18:45:22 -0400

    completed module

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+// src/components/NavBar.jsx
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    // return (
+    //     <nav>
+    //         <ul>
+    //             <li>Home</li>
+    //             <li>Pokemon</li>
+    //         </ul>
+    //     </nav>
+    // );
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/pokemon'>Pokemon</Link>
+                </li>
+                <li>
+                    <Link to="/pokemon/new">New Pokemon</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
